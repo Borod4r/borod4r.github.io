71bdd522e405c99571413f946a21ef7aaffb75d2
Ivan Gadzhega <ivgadzhega@gmail.com>
2024-01-24 17:56:35 +0200

    Add the article about unity git hooks

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1705871378';
+const cacheName = 'chirpy-1706111676';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
